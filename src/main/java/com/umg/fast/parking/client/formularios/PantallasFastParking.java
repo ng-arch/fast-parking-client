929881840b195dccd5f8b78be2d4d775d59be91c
nestor-gaitan <nestor.gaitan@bowpi.com>
2022-05-10 22:36:21 -0600

    CREATE  - first version to client-parking

---
+++ src/main/java/com/umg/fast/parking/client/formularios/PantallasFastParking.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.umg.fast.parking.client.formularios;
+
+
+
+/**
+ *
+ * @author Kevin Barrientos
+ */
+public class PantallasFastParking {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        GenerarTicket vista1 = new GenerarTicket();
+        vista1.setVisible(true);
+        vista1.setLocationRelativeTo(null);
+        vista1.setResizable(false);
+        
+        
+        
+    }
+    
+}
