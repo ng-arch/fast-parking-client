b11acd0271227f2af8334ddf159554fb325f7fb4
Admin <Admin@GONZALEZ-ESCRITORIO>
2022-05-22 22:36:23 -0600

    actualizar

--- src/main/java/com/umg/fast/parking/client/formularios/Registro.java
+++ src/main/java/com/umg/fast/parking/client/formularios/Registro.java
@@ -148,7 +148,7 @@ ClientService clientService = new ClientService();
         request.put("usuario", usuario);
         request.put("password", password);
         
-        JSONObject response = clientService.postRequest("http://localhost:5050/parking/controller/registrousr", request);
+        JSONObject response = clientService.postRequest("http://localhost:5050/parking/controller/register", request);
         JOptionPane.showMessageDialog(this, response.get("mensaje"));
         String success=String.valueOf(response.get("success"));
         if("true".equals(success)){
